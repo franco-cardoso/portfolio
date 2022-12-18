b5474fce6b192b8083b456150978ef87919ff427
franco-cardoso <francolkk788@gmail.com>
2022-12-18 16:06:11 -0300

    filled up info section

--- src/Components/About/About.tsx
+++ src/Components/About/About.tsx
@@ -9,61 +9,63 @@ import { useTranslation } from "react-i18next";
 import MovingLetter from "./MovingLetter";
 
 const About = () => {
-  const { width } = useWindowSize();
-  const containerRef = useRef(null);
-  const scrollPos = useScrollPos(containerRef, false);
-  const { t } = useTranslation();
+    const { width } = useWindowSize();
+    const containerRef = useRef(null);
+    const scrollPos = useScrollPos(containerRef, false);
+    const { t } = useTranslation();
 
-  return (
-    <div className="container" ref={containerRef}>
-      <div className="textWrapper">
-        <div className="textContainer">
-          <div style={{transform:`translateX(${width >= 780 ? scrollPos * 1500 : 0}px)`}}>
-            <h1 className="aboutTitle">
-              {t("about.hello")}
-              <br />
-              {t("about.im.franco")}
-            </h1>
-            <p className="aboutText">
-              <span>
-                {t("about.im.frontend.webdev")}
-                <br /> {t("about.focused.on")}&nbsp;
-              </span>
-              <span className="movingLtrsSpan">
-                {width < 780 ? (
-                  <span className="funkySpan">{t("about.reactive")}</span>
-                ) : (
-                  createMovingWord(t("about.reactive"))
-                )}
-              </span>
-              <span>&nbsp;{t("about.frameworks")}</span>
-            </p>
+    return (
+        <div className="container" ref={containerRef}>
+            <div className="textWrapper">
+                <div className="textContainer">
+                    <div style={{ transform: `translateX(${width >= 780 ? scrollPos * 1500 : 0}px)` }}>
+                        <h1 className="aboutTitle">
+                            {t("about.hello")}
+                            <br />
+                            {t("about.im.franco")}
+                        </h1>
+                        <p className="aboutText">
+                            <span>
+                                {t("about.im.frontend.webdev")}
+                                <br /> {t("about.focused.on")}&nbsp;
+                            </span>
+                            <span className="movingLtrsSpan">
+                                {width < 780 ? (
+                                    <span className="funkySpan">{t("about.reactive")}</span>
+                                ) : (
+                                    createMovingWord(t("about.reactive"))
+                                )}
+                            </span>
+                            <span>&nbsp;{t("about.frameworks")}</span>
+                        </p>
 
-            <div className="skillsetIcons">
-              <SiHtml5 size={40} color="#f16528" className="aboutIcons" aria-label="HTML5 icon" />
-              <SiCss3 size={40} color="#0096d1" className="aboutIcons" aria-label="CSS3 icon" />
-              <SiJavascript size={40} color="#ceb200" className="aboutIcons" aria-label="JavaScript icon" />
-              <SiTypescript size={40} color="#0096d1" className="aboutIcons" aria-label="TypeScript icon" />
-              <FaReact size={40} color="#5dddff" className="aboutIcons" aria-label="React icon" />
+                        <div className="skillsetIcons">
+                            <SiHtml5 size={40} color="#f16528" className="aboutIcons" aria-label="HTML5 icon" />
+                            <SiCss3 size={40} color="#0096d1" className="aboutIcons" aria-label="CSS3 icon" />
+                            {/* prettier-ignore */}
+                            <SiJavascript size={40} color="#ceb200" className="aboutIcons" aria-label="JavaScript icon" />
+                            {/* prettier-ignore */}
+                            <SiTypescript size={40} color="#0096d1" className="aboutIcons" aria-label="TypeScript icon" />
+                            <FaReact size={40} color="#5dddff" className="aboutIcons" aria-label="React icon" />
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-      </div>
 
-      <Info />
-    </div>
-  );
+            <Info />
+        </div>
+    );
 };
 
 const createMovingWord = (word: string) => {
-  const wordSplit = word.split("");
-  return (
-    <span className="funkySpan">
-      {wordSplit.map((l, index) => (
-        <MovingLetter letter={l} key={index} />
-      ))}
-    </span>
-  );
+    const wordSplit = word.split("");
+    return (
+        <span className="funkySpan">
+            {wordSplit.map((l, index) => (
+                <MovingLetter letter={l} key={index} />
+            ))}
+        </span>
+    );
 };
 
 export default About;
--- src/Components/About/Info.tsx
+++ src/Components/About/Info.tsx
@@ -1,57 +1,100 @@
 import { useRef } from "react";
 import { motion, AnimatePresence, useInView } from "framer-motion";
-import './About.css'
+import pfp from "./pfp.jpg";
+import cert from "./cert.webp";
+import "./Info.css";
 
 const Info = () => {
-  const infoRef = useRef(null);
-  const infoInView = useInView(infoRef);
+    const infoRef = useRef(null);
+    const infoInView = useInView(infoRef);
 
-  return (
-    <div className="info">
-      <motion.div
-        className="infoTextWrapper"
-        initial={{ backdropFilter: "brightness(0.1)" }}
-        animate={{
-          backdropFilter: `brightness(${infoInView ? 1 : 0.1})`,
-        }}
-        transition={{ duration: 1, delay: 0.7 }}
-      >
-        <AnimatePresence> 
-          {infoInView && (
-            <motion.div
-              className="infoTextAnim"
-              initial={{ x: -600, filter: "opacity(0)" }}
-              animate={{ x: 0, filter: "opacity(1)" }}
-              exit={{ x: -600, filter: "opacity(0)" }}
-              transition={{ duration: 1, delay: 0.7 }}
-            >
-              <div>
-                <h1 className="aboutTitle" style={{ textAlign: "start" }}>
-                  Lorem ipsum dolor sit amet consectetur.
-                </h1>
-                <p
-                  className="aboutText"
-                  style={{
-                    color: "#fff",
-                    textAlign: "start",
-                    margin: "10px 0",
-                  }}
-                >
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi
-                  quia, vero molestias accusamus voluptate ipsum dolore numquam
-                  reiciendis?
-                </p>
-              </div>
-            </motion.div>
-          )}
-        </AnimatePresence>
-        <div
-          style={{ width: "300px", height: "300px", position: "absolute" }}
-          ref={infoRef}
-        ></div>
-      </motion.div>
-    </div>
-  );
+    return (
+        <section className="info">
+            <div className="infoWrapper">
+                <div className="infoHeader">
+                    <img className="infoPic" src={pfp} alt="Profile picture" />
+                    <div className="infoTitle">
+                        <h2>Franco Cardoso</h2>
+                        <hr />
+                        <p className="infoSub">Full Stack Web Developer</p>
+                        <p className="infoSubSub">Graduated at Numen Academy</p>
+                    </div>
+                </div>
+
+                <div className="infoText">
+                    <p>
+                        Hello, my name is Franco and I am a full stack web developer with a passion for creating
+                        beautiful and functional websites and applications. With a strong foundation in both front-end
+                        and back-end development, I am able to bring projects from concept to launch with ease. My
+                        skills include proficiency in languages such as <strong>HTML</strong>, <strong>CSS</strong>,{" "}
+                        <strong>JavaScript</strong>, <strong>SQL</strong>, <strong>Python</strong>, front-end frameworks
+                        like <strong>React</strong>, as well as back-end services such as <strong>MongoDB</strong>{" "}
+                        databases and <strong>Node.js</strong> servers.
+                    </p>
+
+                    <p>
+                        Whether it's building responsive websites, developing scalable web applications, or integrating
+                        with APIs, I have the skills to take on a variety of projects, as well as high adaptability when
+                        learning new technologies.
+                    </p>
+
+                    <p>
+                        Graduated at Numen Academy, I recieved a degree in Full Stack Web Development, backed by the
+                        Argentine Atlantis University
+                    </p>
+                </div>
+
+                <div className="certificate">
+                    <img src={cert} alt="" width="280px" height="190px" />
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default Info;
+
+{
+    /* <motion.div
+  className="infoTextWrapper"
+  initial={{ backdropFilter: "brightness(0.1)" }}
+  animate={{
+    backdropFilter: `brightness(${infoInView ? 1 : 0.1})`,
+  }}
+  transition={{ duration: 1, delay: 0.7 }}
+>
+  <AnimatePresence> 
+    {infoInView && (
+      <motion.div
+        className="infoTextAnim"
+        initial={{ x: -600, filter: "opacity(0)" }}
+        animate={{ x: 0, filter: "opacity(1)" }}
+        exit={{ x: -600, filter: "opacity(0)" }}
+        transition={{ duration: 1, delay: 0.7 }}
+      >
+        <div>
+          <h1 className="aboutTitle" style={{ textAlign: "start" }}>
+            Lorem ipsum dolor sit amet consectetur.
+          </h1>
+          <p
+            className="aboutText"
+            style={{
+              color: "#fff",
+              textAlign: "start",
+              margin: "10px 0",
+            }}
+          >
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi
+            quia, vero molestias accusamus voluptate ipsum dolore numquam
+            reiciendis?
+          </p>
+        </div>
+      </motion.div>
+    )}
+  </AnimatePresence>
+  <div
+    style={{ width: "300px", height: "300px", position: "absolute" }}
+    ref={infoRef}
+  ></div>
+</motion.div> */
+}
