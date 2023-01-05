57c21232867eccf07bd4eb1fbf6173048de562c8
franco-cardoso <francolkk788@gmail.com>
2023-01-05 17:25:58 -0300

    added react-training link

--- src/Components/Webapps/WebAppsHub.tsx
+++ src/Components/Webapps/WebAppsHub.tsx
@@ -3,20 +3,28 @@ import "./WebApps.css";
 import Wbapp from "./Wbapp";
 
 const WebApps = () => {
-  const { t } = useTranslation(); 
+    const { t } = useTranslation();
 
-  return (
-    <div className="container">
-      <a href="https://premiumbakery.vercel.app/">
-        <Wbapp
-          img="https://raw.githubusercontent.com/franco-cardoso/portfolio/main/src/Assets/img/wbappbakery.webp"
-          title={t("webapps.titleBakery")}
-          subTitle={t("webapps.subBakery")}
-          desc={t("webapps.descBakery")}
-        />
-      </a>
-    </div>
-  );
+    return (
+        <div className="container">
+            <a href="https://premiumbakery.vercel.app/" target={"_blank"} rel="noreferrer">
+                <Wbapp
+                    img="https://raw.githubusercontent.com/franco-cardoso/portfolio/main/src/Assets/img/wbappbakery.webp"
+                    title={t("webapps.titleBakery")}
+                    subTitle={t("webapps.subBakery")}
+                    desc={t("webapps.descBakery")}
+                />
+            </a>
+            <a href="https://franco-cardoso.github.io/react-training/" target={"_blank"} rel="noreferrer">
+                <Wbapp
+                    img="https://www.solidbackgrounds.com/images/1920x1080/1920x1080-gray-solid-color-background.jpg"
+                    title={"Practice"}
+                    subTitle={"React Project"}
+                    desc={"A collection of web components made for practicing."}
+                />
+            </a>
+        </div>
+    );
 };
 
 export default WebApps;
