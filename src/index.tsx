d4dfa2773b41fc27d4003a07432512647c46576e
franco-cardoso <francolkk788@gmail.com>
2023-01-11 15:06:41 -0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { HashRouter } from 'react-router-dom';
-import './i18n/i18n'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
