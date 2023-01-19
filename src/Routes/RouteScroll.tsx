9b1bfe8d10956a6366e4b5adef67fc320fa6d993
franco-cardoso <francolkk788@gmail.com>
2023-01-18 21:42:52 -0300

    removed unused code and comments

--- src/Routes/RouteScroll.tsx
+++ src/Routes/RouteScroll.tsx
@@ -9,7 +9,6 @@ const RouteScroll = () => {
 
     return (
         <motion.main
-            style={{ height: "100%", width: "100%" }}
             initial={{ x: 1000, opacity: 0 }}
             animate={{ x: 0, opacity: 1 }}
             exit={{ x: -1000, opacity: 0 }}
