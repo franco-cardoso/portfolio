7d56ed6172dd985e34560b45d6ef190dc4868afc
franco-cardoso <francolkk788@gmail.com>
2023-06-02 01:28:45 -0300

    changed an app background

--- src/Components/Webapps/WebAppsHub.tsx
+++ src/Components/Webapps/WebAppsHub.tsx
@@ -17,7 +17,7 @@ const WebAppsHub = () => {
             </a>
             <a href="https://raincalls.com/" target={"_blank"} rel="noreferrer">
                 <Wbapp
-                    img="https://raw.githubusercontent.com/franco-cardoso/portfolio/fabe55a388e6a139fd6ce3a71fd90f9dbb492bca/src/Assets/img/raincalls4.webp"
+                    img="https://raw.githubusercontent.com/franco-cardoso/portfolio/main/src/Assets/img/raincalls4.webp"
                     title={t("webapps.titleRaincalls")}
                     subTitle={t("webapps.subRaincalls")}
                     desc={t("webapps.descRaincalls")}
@@ -25,7 +25,7 @@ const WebAppsHub = () => {
             </a>
             <a href="https://franco-cardoso.github.io/react-training/" target={"_blank"} rel="noreferrer">
                 <Wbapp
-                    img="https://www.solidbackgrounds.com/images/1920x1080/1920x1080-gray-solid-color-background.jpg"
+                    img="https://raw.githubusercontent.com/franco-cardoso/portfolio/main/src/Assets/img/practiceapp.png"
                     title={t("webapps.titlePractice")}
                     subTitle={t("webapps.subPractice")}
                     desc={t("webapps.descPractice")}
