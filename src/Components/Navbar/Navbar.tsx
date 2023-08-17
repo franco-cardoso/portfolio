b4f92c77997d5da5bf7a6077a56c3e3b446a102e
franco-cardoso <francolkk788@gmail.com>
2023-08-16 22:44:23 -0300

    logo

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -5,44 +5,47 @@ import MobileMenu from "./MobileMenu";
 import NavbarButtons from "./NavbarButtons";
 import SocialMedia from "./SocialMedia";
 import i18n from "../../i18n/i18n";
-import {useTranslation} from "react-i18next"
-import { Btn } from "./NavbarButtons";
+import { useTranslation } from "react-i18next";
+import { ReactComponent as Logo } from "../../Assets/img/logo-white.svg";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  const { width } = useWindowSize();
-  const { t } = useTranslation();
+    const { width } = useWindowSize();
+    const { t } = useTranslation();
 
-  return (
-    <>
-      {width >= 1024 && (
-        <motion.nav
-          initial={{ x: -600 }}
-          animate={{ x: 0 }}
-          transition={{ duration: 1.2 }}
-        >
-          <div className="navbarContainer">
-            <div className="somethingContainer">
-              <Btn onClick={() => i18n.changeLanguage("en")}>en</Btn>
-              <Btn onClick={() => i18n.changeLanguage("es")}>es</Btn>
-            </div>
+    // for some reason t() isnt working unless i18n.changeLanguage()
+    //  is called somewhere in the code, even if not executed
+    // eslint-disable-next-line
+    const what = () => i18n.changeLanguage("en");
 
-            <div className="navbarBtnsContainer">
-              <NavbarButtons
-                btns={[
-                  { text: t("navbar.home"), path: "/" },
-                  { text: t("navbar.webapps"), path: "/webapps" },
-                  { text: t("navbar.contact"), path: "/contact" },
-                ]}
-              />
-            </div>
-                
-            <SocialMedia />
-          </div>
-        </motion.nav>
-      )}
-      {width < 1024 && <MobileMenu />}
-    </>
-  );
+    return (
+        <>
+            {width >= 1024 && (
+                <motion.nav initial={{ x: -600 }} animate={{ x: 0 }} transition={{ duration: 1.2 }}>
+                    <div className="navbarContainer">
+                        <div className="somethingContainer">
+                            <Link to="/">
+                                <Logo className="logo"></Logo>
+                            </Link>
+                        </div>
+
+                        <div className="navbarBtnsContainer">
+                            <NavbarButtons
+                                btns={[
+                                    { text: t("navbar.home"), path: "/" },
+                                    { text: t("navbar.webapps"), path: "/webapps" },
+                                    { text: t("navbar.contact"), path: "/contact" },
+                                ]}
+                            />
+                        </div>
+
+                        <SocialMedia />
+                    </div>
+                </motion.nav>
+            )}
+            {width < 1024 && <MobileMenu />}
+        </>
+    );
 };
 
 export default Navbar;
